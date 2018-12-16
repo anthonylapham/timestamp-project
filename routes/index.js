6d49e03c41aecf58ddff60ece8e661a49dba8558
Anthony Lapham <lapham.2@wright.edu>
2018-12-16 10:21:19 -0500

    unix

--- routes/index.js
+++ routes/index.js
@@ -6,27 +6,46 @@ router.get('/', (req, res) => {
 });
 
 router.get('/api/timestamp', (req, res) => {
-  // for empty date strings, get the current date/time
-  if(date_string === null){
-    return currentDate;
-  }
   // Date.now() => current time in ms
   // new Date() => a new date object. with lots of properties and methods
-  const currentDate = newDate();
-  let date = currentDate.getDate();
-  let month = currentDate.getMonth();
-  let year = currentDate.getFullYear();
+  function date_string(string = null) {
+    if (!string) {
+      const now = new Date();
+      const date = now.getDate();
+      const month = now.getMonth() + 1;
+      const year = now.getFullYear();
+      const unix = now.getTime();
+      const natural = `${month}-${date}-${year}`;
+      return { natural, unix }
+    }
 
-  const date_string = date + "-" + (month + 1) + "-" + year;
+    // do we have a date string
+    let givenDate = null;
+    try {
+      givenDate = new Date(string);
+    } catch (e) {
+      const givenUnix = parseInt(string, 10);
+      if (!isNaN(givenUnix)) {
+        givenDate = new Date(givenUnix);
+      }
+      return { err: 'Invalid Date' }
+    }
+    const date = givenDate.getDate();
+    const month = givenDate.getMonth() + 1;
+    const year = givenDate.getFullYear();
+    const unix = givenDate.getTime();
+    const natural = `${month}-${date}-${year}`;
+    return { natural, unix }
+  }
 
-  res.send(date_string);
-  console.log(date_string.toLocaleDateString('en-US'));
+  // if ?date_string is empty, then return current time
+  let response = null;
+  if (req.query && req.query.date_string) {
+    response = date_string(req.query.date_string);
+  } else {
+    response = date_string();
+  }
+  res.send(response);
 });
 
-router.get('/api/timestamp/:date_string', (req, res) => {
-  // use req.params.date_string for the value
-  // check if it is valid or not
-  // if it is valid, determine if it is MS or YYYY-MM-DD
-  // if it is MS, req.params.date_string will be a string, so you will have to parseInt to make make it a number
-});
 module.exports = router;
